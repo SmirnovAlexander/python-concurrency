07aad09d2a5db213b0826f6a0e194f32a9bd2797
Alexander Smirnov <furiousteabag@gmail.com>
2023-04-18 12:28:54 +0700

    add threading example

---
+++ 02_threading/01_thread_creation.py
@@ -0,0 +1,20 @@
+import threading
+from threading import current_thread
+
+
+def thread_tasks(a, b, c, key1, key2):
+    print(f"{current_thread().getName()} received args: {a}, {b}, {c}, {key1}, {key2}")
+
+
+thread = threading.Thread(
+    group=None,
+    target=thread_tasks,
+    name="DemoThread",
+    args=(1, 2, 3),
+    kwargs={"key1": 777, "key2": 111},
+    daemon=None,
+)
+
+print(f"Current thread: {current_thread().getName()}")
+thread.start()
+thread.join()
